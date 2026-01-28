b43703e169839bba1dc0f535084f98eb2a6bec1f
josepuf <joseph.trumpy@gmail.com>
2026-01-28 08:56:44 -0600

    new branch

--- prog705x.java
+++ prog705x.java
@@ -1,6 +1,14 @@
+import java.io.*;
+import java.util.*;
 public class prog705x
-{public static void main(String [] args)
+{public static void main(String [] args) throws Exception 
     {
+        Scanner input = new Scanner(new File("705x"));
+        Scanner input2 = new Scanner(new File("705xnumbers"));
+
+        ArrayList<String> Joe = new ArrayList<String>();
+        ArrayList<Integer> Jack = new ArrayList<Integer>();
+
         
     }
 }
