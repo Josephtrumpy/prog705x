9f2c6ee1425e4e4907a6862c4be12367f928ab52
josepuf <joseph.trumpy@gmail.com>
2026-01-28 08:57:55 -0600

    jlbij

--- prog705x.java
+++ prog705x.java
@@ -1,13 +1,14 @@
 import java.io.*;
 import java.util.*;
-public class prog705x
-{public static void main(String [] args) throws Exception 
+public class prog705x {
+    public static void main(String [] args) throws Exception 
     {
         Scanner input = new Scanner(new File("705x"));
         Scanner input2 = new Scanner(new File("705xnumbers"));
 
-        ArrayList<String> Joe = new ArrayList<String>();
-        ArrayList<Integer> Jack = new ArrayList<Integer>();
+        System.out.println("Jack");
+        System.out.println("Carter");
+        System.out.println("Brayden");
 
         
     }
