23ed27b2fc4d771b3da80669e8954b93da20d282
Joseph Trumpy <148014641+Josephtrumpy@users.noreply.github.com>
2026-02-24 16:53:32 -0600

    Update prog705x.java

--- prog705x.java
+++ prog705x.java
@@ -3,13 +3,45 @@ import java.util.*;
 public class prog705x {
     public static void main(String [] args) throws Exception 
     {
-        Scanner input = new Scanner(new File("705x"));
-        Scanner input2 = new Scanner(new File("705xnumbers"));
+        Scanner poemfile = new Scanner(new File("prog512h.dat"));
+        ArrayList<String> poemLines = new ArrayList<>();
 
-        System.out.println("Jack");
-        System.out.println("Carter");
-        System.out.println("Brayden");
+        while(poemfile.hasNextLine())
+            {
+                poemLines.add(poemfile.nextLine());
+            }
 
+        poemfile.close();
+
+        Scanner codeFile = new Scanner(new File("prog705x.txt"));
+        ArrayList<String> codes = new ArrayList<>();
+
+        while(codeFile.hasNextLine())
+            {
+                codes.add(codeFile.nextLine());
+            }
+           codeFile.close();
+
+
+         String magicword = "";
+
+                for(String code : codes)
+                {
+                    int lineNum = Integer.parseInt(code.substring(0,2));
+                    int wordNum = Integer.parseInt(code.substring(2,3));
+                    int charNum = Integer.parseInt(code.substring(3,4));
+
+                    String line = poemLines.get(lineNum -1);
+                    String[] words = line.split(" ");
+                    String word = words[wordNum-1];
+                    String letter = word.substring(charNum-1,charNum);
+
+                    magicword = magicword + letter;
+                }
+
+                System.out.println("MagicWord: " + magicword);
+                
+                
         
     }
-}
+  }    
